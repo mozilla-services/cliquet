9e6036a8d4060f36e920cc1d304a486bc29fba0b
Alexis Métaireau <alexis@notmyidea.org>
2015-02-19 22:38:46 +0100

    Add github cliquet URL to the deps.

--- setup.py
+++ setup.py
@@ -19,6 +19,11 @@ ENTRY_POINTS = {
         'main = readinglist:main',
     ]}
 
+DEPENDENCY_LINKS = [
+    'https://github.com/mozilla-services/cliquet/tarball/cliquet'
+    '#egg=cliquet-0.3.dev0',
+]
+
 
 setup(name='readinglist',
       version='0.3.dev0',
@@ -37,4 +42,5 @@ setup(name='readinglist',
       include_package_data=True,
       zip_safe=False,
       install_requires=REQUIREMENTS,
-      entry_points=ENTRY_POINTS)
+      entry_points=ENTRY_POINTS,
+      dependency_links=DEPENDENCY_LINKS)
