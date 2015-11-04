95a408e55eb89bda6e104f4a6bef0e25898f9dea
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-11-04 11:19:23 +0100

    Add a test for the hello page.

--- cliquet/tests/resource/test_views.py
+++ cliquet/tests/resource/test_views.py
@@ -655,6 +655,15 @@ class VaryHeaderTest(BaseWebTest, unittest.TestCase):
         settings['toadstool_read_principals'] = 'system.Everyone'
         return settings
 
+    def test_vary_header_is_not_set_if_anonymous_on_hello_page(self):
+        resp = self.app.get("/")
+        self.assertNotIn('Vary', resp.headers)
+
+    def test_vary_header_is_set_if_authenticated_on_hello_page(self):
+        resp = self.app.get("/", headers=self.headers)
+        self.assertIn('Vary', resp.headers)
+        self.assertIn('Authorization', resp.headers['Vary'])
+
     def test_vary_header_is_not_set_if_anonymous_on_collection_url(self):
         resp = self.app.get(self.collection_url)
         self.assertNotIn('Vary', resp.headers)
